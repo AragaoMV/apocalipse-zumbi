1b0a4332d86547b3a84c31bc636f09271f9c0b46
AragaoMV <marcosvinisa@icloud.com>
2023-06-16 10:44:12 -0300

    Otimização Spawners

--- apocalipse-zumbi-jogo/Assets/Scripts/GeradorZumbis.cs
+++ apocalipse-zumbi-jogo/Assets/Scripts/GeradorZumbis.cs
@@ -7,21 +7,50 @@ public class GeradorZumbis : MonoBehaviour
     public GameObject Zumbi;
     float contaTempo = 0;
     public float TempoGeraZumbi = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public LayerMask LayerZumbi;
+    private float rangeDeGeracao = 5;
+    private float distanciaJogadorInimigo = 20;
+    private GameObject jogador;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        jogador = GameObject.FindWithTag("Player");
+    }
     void Update()
     {
-        contaTempo += Time.deltaTime;
-        if (contaTempo>= TempoGeraZumbi)
+        if (Vector3.Distance(transform.position, jogador.transform.position) > distanciaJogadorInimigo)
         {
-            Instantiate(Zumbi, transform.position, transform.rotation); 
-            contaTempo = 0;
-        }       
-        
+            contaTempo += Time.deltaTime;
+            if (contaTempo >= TempoGeraZumbi)
+            {
+                StartCoroutine(GeraZumbi());
+                contaTempo = 0;
+            }
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, rangeDeGeracao);
+    }
+    IEnumerator GeraZumbi()
+    {
+        Vector3 novoSpawn = AleatorizaSpawn();
+        Collider[] colisores = Physics.OverlapSphere(novoSpawn, 1, LayerZumbi);
+        while (colisores.Length > 0)
+        {
+            novoSpawn = AleatorizaSpawn();
+            colisores = Physics.OverlapSphere(novoSpawn, 1, LayerZumbi);
+            yield return null;
+        }
+        Instantiate(Zumbi, novoSpawn, transform.rotation);
+    }
+    Vector3 AleatorizaSpawn()
+    {
+        Vector3 posicao = Random.insideUnitSphere * rangeDeGeracao;
+        posicao += transform.position;
+        posicao.y = 0;
+
+        return posicao;
     }
 }
